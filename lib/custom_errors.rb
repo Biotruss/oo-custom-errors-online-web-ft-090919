0b367f182f5005f7e9ce243a547adc376737eea8
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-28 07:58:34 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,10 +8,13 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person 
-      raise PartnerError
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
     else person.pertner = self
     end
-    person.partner = self
   end
   
   class PartnerError < StandardError
